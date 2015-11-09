cd8188a04454ac07e23f150d046cd4cd0ff4a561
noifuji <ryoma1025@gmail.com>
2015-11-09 05:58:37 +0000

    logger default

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/entry')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , morgan = require('morgan');
 
 var app = express();
 
@@ -18,7 +19,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
