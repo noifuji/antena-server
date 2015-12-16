ec6de0641eb1a59e01a3b5434f9277dda98c41ca
noifuji <ryoma1025@gmail.com>
2015-12-16 15:53:42 +0000

    update cronjob

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 var async = require('async');
 var request = require('request');
 var CronJob = require('cron').CronJob;
-var FeedParser = require('feedparser');
+var feedparser = require('ortoo-feedparser');
+var model = require('./model/model.js');
+var Entries = model.Entries;
+var easyimg = require('easyimage');
+var fs = require('fs');
 
 var urlList = [
     //ニュース
@@ -108,53 +112,159 @@ var urlList = [
     }
 ];
 
-new CronJob('0,20,40 * * * * *', function() {
+var job = new CronJob('0/1 * * * * *', function() {
+
+    console.log(new Date());
 
     var counter = 0;
 
     async.eachSeries(urlList, function(url, callback) {
+        
+        var entries = [];
 
-        var req = request({
+        request({
             method: 'GET',
             url: url.url,
-            encoding: null
+            encoding: null,
+            timeout:500
+        }, function(error, response, body) {
+            if(error) {
+                console.log("error : " + error);
+                callback(null, null);
+                return;
+            }
+            // body is the decompressed response body 
+            //console.log('status code is: ' + JSON.parse(parser.toJson(body)).rdf.channel.title);
+            feedparser.parseString(body, function(e, m ,a) {
+                if (error || !m || !a) {
+                    console.error(error);
+                    callback(null,null);
+                    return;
+                }
+                //console.log(m.title);
+                
+                for(var i = 0; i < a.length; i++) {
+                    var thumnailUrlList = a[i]["content:encoded"]["#"].match(/(http:){1}[\S_-]+((\.png)|(\.jpg)|(\.JPG)|(\.gif))/);
+                            var thumnail;
+                            if (thumnailUrlList != null) {
+                                thumnail = thumnailUrlList[0];
+                            }
+                            else {
+                                //thumnail = "https://antena-noifuji.c9.io/images/default.png";
+                                thumnail = "";
+                            }
+                            var pubDate = new Date(a[i].pubDate).getTime();
+                            var now = new Date().getTime();
+                            var ep = {
+                                'title': a[i].title,
+                                'url': a[i].link,
+                                'publicationDate': (pubDate > now ? now : pubDate),//何故か日付が未来の場合があるので修正する
+                                'thumbnail': thumnail,
+                                'description': a[i].description
+                            };
+                            entries.push(ep);
+                }
+                
+                async.each(entries, function(e, entryCallback) {
+                            var entry = new Entries();
+                            entry.title = e.title;
+                            entry.url = e.url;
+                            entry.publicationDate = e.publicationDate;
+                            entry.thumbnail = e.thumbnail;
+                            entry.sitetitle = m.title;
+                            entry.description = e.description;
+                            entry.category = url.category;
+
+                            Entries.find({
+                                url: entry.url
+                            }, function(err, docs) {
+                                if (docs.length == 0) {
+                                    console.log((new Date()) + " : <" + entry.sitetitle + "> " + entry.title);
+                                    entry.save(function(err) {
+                                        if (err) {
+                                            console.log(err);
+                                        }
+                                        else {
+                                            if (entry.thumbnail != "") {
+                                                request({
+                                                        method: 'GET',
+                                                        url: entry.thumbnail,
+                                                        encoding: null
+                                                    },
+                                                    function(error, response, body) {
+                                                        var filename = getFilename(entry.thumbnail);
+                                                        if (!error && response.statusCode === 200) {
+                                                            fs.writeFileSync('./img/' + filename, body, 'binary');
+                                                            
+                                                            easyimg.thumbnail({
+                                                                src: './img/' + filename,
+                                                                dst: './thumbnail/' + filename,
+                                                                width: 144,
+                                                                height: 144,
+                                                                x: 0,
+                                                                y: 0
+                                                            }).then(
+                                                                function(image) {
+                                                                    console.log('Resized and cropped: ' + image.width + ' x ' + image.height);
+                                                                    easyimg.convert({
+                                                                        src: './thumbnail/' + image.name,
+                                                                        dst: './thumbnail/' + splitExt(image.name)[0] + '.png',
+                                                                        quality: 10
+                                                                    }).then(function(file) {
+                                                                        console.log("converted filename : " + file.name);
+                                                                        fs.unlink('./thumbnail/' + image.name, function(err) {
+                                                                            if (err) {
+                                                                                console.log(err);
+                                                                            };
+                                                                            console.log('successfully deleted');
+                                                                        });
+                                                                    });
+                                                                },
+                                                                function(err) {
+                                                                    console.log(err);
+                                                                }
+                                                            );
+                                                        }
+
+                                                    }
+                                                );
+                                            }
+                                        }
+                                    });
+                                }
+                                entryCallback(null, null);
+                            });
+                        }, function(err, results) {
+                            if (err) {
+                                throw err;
+                            }
+                            //console.log('The entry has checked.');
+
+                            entries = [];
+                            counter++;
+                            callback(null, null);
+                        });
+            });
+            
         });
-        var feedparser = new FeedParser();
-
-        req.on('error', function(error) {
-            console.log(error);
-        });
-        req.on('response', function(res) {
-            var stream = this;
-
-            if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
-
-            stream.pipe(feedparser);
-        });
-
-        feedparser.on('error', function(error) {
-            // always handle errors
-        });
-        feedparser.on('readable', function() {
-            // This is where the action is!
-            var stream = this,
-                meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
-                ,
-                item;
-
-            //console.log(meta.link);
-
-            counter++;
-            callback(null, null);
-        });
-
-
     }, function(err, results) {
         if (err) {
             console.log("error occured");
             throw err;
         }
         console.log('each all done. ' + counter);
+        console.log(new Date());
     });
 
 }, null, true, "Japan");
+
+function getFilename(url) {
+    var temp = url.split('/');
+    return temp[temp.length - 1];
+}
+
+function splitExt(filename) {
+    return filename.split(/\.(?=[^.]+$)/);
+}
+
+job.start();
