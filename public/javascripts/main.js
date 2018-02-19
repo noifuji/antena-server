9556fe2bc25bb11d9b341129367a2863cf259a31
noifuji <ryoma1025@gmail.com>
2018-02-19 11:08:30 +0000

    aaa

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,7 +10,9 @@ antena.controller('MainController', ['$scope', '$http', '$sce', function($scope,
     console.log(protocol);
     var host = location.hostname;
     console.log(host);
-    $http.get(protocol+"//"+host+"/entry?time=0&category=", config)
+    var port = location.port;
+    console.log(port);
+    $http.get(protocol+"//"+host+":"+port+"/entry?time=0&category=", config)
         .success(function(data, status, headers, config) {
             for(var i = 0; i < data.entries.length; i++) {
                 data.entries[i].publicationDate = toLocaleString(new Date(data.entries[i].publicationDate));
@@ -24,7 +26,7 @@ antena.controller('MainController', ['$scope', '$http', '$sce', function($scope,
 
     $scope.movetoEntry = function(index) {
         //window.open($scope.entries[index].url);
-        $http.get(protocol+"//"+host+"/description?id="+$scope.entries[index]._id, config)
+        $http.get(protocol+"//"+host+":"+port+"/description?id="+$scope.entries[index]._id, config)
         .success(function(data, status, headers, config) {
             console.log(data.entry.description);
             $scope.content = $sce.trustAsHtml(data.entry.description);
